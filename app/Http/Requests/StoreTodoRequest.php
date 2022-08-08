fe11f7775c1f21b004866ae4635a43a11e17f9b4
timalsina-yuvraj <timalsinayuvraj@gmail.com>
2022-08-08 14:54:41 +0545

    changes

--- app/Http/Requests/StoreTodoRequest.php
+++ app/Http/Requests/StoreTodoRequest.php
@@ -24,7 +24,7 @@ class StoreTodoRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'=>'required|unique:todos',
+            'title'=>'required|unique:todos,title',
             'description'=>'required'
         ];
     }
--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -14,6 +14,10 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'id'=>$this->id,
+          'title'=>$this->title,
+          'description'=>$this->description,
+        ];
     }
 }
